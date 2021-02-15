8ad3a946f115a136b56a76b0250510075f5c33f4
A Ruiz <luis.alonso.16@hotmail.com>
2021-02-14 19:14:30 -0700

    Improve linter package comments

--- ddderr.go
+++ ddderr.go
@@ -1,6 +1,7 @@
-/*
- DDD Error is a generic domain-driven exception wrapper made for Go.
- DDD Error aims to ease the lack of exception handling in The Go Programming Language by defining a wide selection of common exceptions which happen inside the domain and/or infrastructure layer(s).
- 	DDD Error is dependency-free, it complies with Go's built-in error interface and avoids reflection to increase overall performance.
-*/
+// Package ddderr -or DDD Error- is a generic domain-driven exception wrapper made for Go.
+//
+// DDD Error aims to ease the lack of exception handling in The Go Programming Language by
+// defining a wide selection of common exceptions which happen inside the domain and/or infrastructure layer(s).
+//
+//	DDD Error is dependency-free, it complies with Go's built-in error interface and avoids reflection to increase overall performance.
 package ddderr
